f928dfd424fc4d59873c61c5d2cb4b3ceb5b1c29
DercilioFontes <derciliofontes@gmail.com>
2019-02-13 00:26:56 -0800

    Implemented the footer and few changes.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa";
+import dafdevImage from "./assets/img/dafdev.png";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -18,15 +20,35 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>React Counter App</h1>
       </header>
-      <div className="App-buttons">
-        <Button increment={1} onClickFunction={incrementCount} />
-        <Button increment={10} onClickFunction={incrementCount} />
-        <Button increment={100} onClickFunction={incrementCount} />
-        <Button increment={1000} onClickFunction={incrementCount} />
-      </div>
-      <h1>
-        <span>{count}</span>
-      </h1>
+      <main>
+        <div className="App-buttons">
+          <Button increment={1} onClickFunction={incrementCount} />
+          <Button increment={10} onClickFunction={incrementCount} />
+          <Button increment={100} onClickFunction={incrementCount} />
+          <Button increment={1000} onClickFunction={incrementCount} />
+        </div>
+        <h1>
+          <span>{count}</span>
+        </h1>
+      </main>
+      <footer>
+        <a
+          className="not-active"
+          href="https://github.com/DercilioFontes/react-counter-dafdev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="github" />
+        </a>
+        <code>dafdev</code>
+        <a
+          href="https://derciliofontes.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img id="dafdev" src={dafdevImage} alt={"dafdev"} />
+        </a>
+      </footer>
     </div>
   );
 };
